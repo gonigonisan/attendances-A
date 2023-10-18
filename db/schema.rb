ac61bcf6b331d6870af1f25660ba17ade246091c
HITOMI <pankun0321@gmail.com>
2023-10-18 19:36:15 +0000

    Finish edit-attendances

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20231003201417) do
+ActiveRecord::Schema.define(version: 20231016191239) do
 
   create_table "attendances", force: :cascade do |t|
     t.date "worked_on"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20231003201417) do
     t.datetime "updated_at", null: false
     t.string "overtime"
     t.string "banksmen"
+    t.boolean "next_day"
     t.index ["user_id"], name: "index_attendances_on_user_id"
   end
 
